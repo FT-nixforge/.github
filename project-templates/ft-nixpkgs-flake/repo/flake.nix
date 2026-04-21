39b2332cbb08be1fa85e337dba4a40b5c5fb9f04
FTMahringer <fynn.mahringer@outlook.com>
2026-04-21 12:30:41 +0200

    fix: removal of templates and such (not needed)

--- project-templates/ft-nixpkgs-flake/repo/flake.nix
+++
@@ -1,9 +0,0 @@
-{
-  "description": "{{FLAKE_NAME}} — {{FLAKE_DESC}}",
-  "inputs": {
-    "nixpkgs": {
-      "url": "github:NixOS/nixpkgs/nixos-unstable"
-    }
-  },
-  "outputs": "outputs = { self, nixpkgs, ... }: { }"
-}
